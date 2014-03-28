4ab5184c5623d7db5c7ecfad51f05ea63b6dc389
Angel <ese.acg@gmail.com>
2014-03-28 11:53:40 +0100

    añadido random al 0092

--- Responder.java
+++ Responder.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Random;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -10,6 +12,9 @@ import java.util.*;
 public class Responder
 {   
     private HashMap<String, String> hm;
+
+    private Random aleatorio;
+    private ArrayList<String> respuestas;
     /**
      * Construct a Responder - nothing to do
      */
@@ -20,23 +25,42 @@ public class Responder
         hm.put("conexion", "Arreglaremos esa conexion");
         hm.put("error", "Reinicie el equipo, para ver si remite el error");
         hm.put("factura", "Revisaremos su factura");
+
+        aleatorio = new Random();
+        respuestas = new ArrayList<>();
+        respuestas.add("Me parece serio");
+        respuestas.add("Nos pondremos con ello");
+        respuestas.add("Reinicie el equipo");
+        respuestas.add("Problemas en el pago");
+        respuestas.add("Me gustaría poder ayudarle");
+        respuestas.add("No se enfade");
     }
-    
+
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
     public String generateResponse(String entrada)
-    {   
-        for(String key: hm.keySet()){
-            if(entrada.contains(key)){                
-                return hm.get(key); 
-            }
+    {
+        String respuesta = "";        
+        if(hm.containsKey(entrada) ){
+            respuesta = hm.get(entrada);    
+        }else{
+            int numeroObtenido = aleatorio.nextInt(respuestas.size());
+            respuesta = respuestas.get(numeroObtenido); 
         }
+        return respuesta;
         
-       /* if(hm.containsKey(entrada) ){
-            return hm.get(entrada);    
-        }*/        
-        return "That sounds interesting. Tell me more...";
+        
+        /**
+         * for(String key: hm.keySet()){
+         *      if(entrada.contains(key)){ 
+         *           return hm.get(key);
+         *      }
+         * }
+         *  return "That sounds interesting. Tell me more...";
+         */
+
+        
     }
 }
